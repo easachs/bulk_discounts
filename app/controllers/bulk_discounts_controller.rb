399750f15f5325f16c0ea0ed76909465f37333cc
Eli Sachs <easachs13@gmail.com>
2022-08-04 17:56:06 -0600

    Add discount new/create feats

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,11 +1,27 @@
 class BulkDiscountsController < ApplicationController
+  before_action :find_merchant, only: [:index, :new, :create]
 
   def index
-    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show
     @discount = BulkDiscount.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @merchant.bulk_discounts.create!(discount_params)
+    redirect_to merchant_bulk_discounts_path(@merchant)
+  end
+
+  private
+  def discount_params
+    params.permit(:percent, :quantity)
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
 end
