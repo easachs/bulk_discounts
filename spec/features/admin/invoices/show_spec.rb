03b0ba92fbae5a2c0de724d4981f74540563d94f
Eli Sachs <easachs13@gmail.com>
2022-08-05 22:46:47 -0600

    Improve discounted revenue specs

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -69,4 +69,29 @@ describe 'Admin Invoices Index Page' do
       expect(@i1.status).to eq('complete')
     end
   end
+
+  it "shows the total and discounted revenue" do
+    merchant_1 = Merchant.create!(name: 'Hair Care')
+    merchant_2 = Merchant.create!(name: 'Eye Care')
+    item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant_1.id, status: 1)
+    item_2 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: merchant_1.id)
+    item_3 = Item.create!(name: "Glasses", description: "Helps you see good", unit_price: 20, merchant_id: merchant_2.id)
+    item_4 = Item.create!(name: "Sunglasses", description: "Sun is bright", unit_price: 15, merchant_id: merchant_2.id)
+    customer = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+    invoice = Invoice.create!(customer_id: customer.id, status: 2, created_at: "2012-03-27 14:54:09")
+    ii_1 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item_1.id, quantity: 10, unit_price: 10, status: 2) # 100 -> 75 +
+    ii_2 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item_2.id, quantity: 20, unit_price: 10, status: 1) # 200 -> 100 +
+    ii_3 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item_3.id, quantity: 10, unit_price: 10, status: 1) # 100 +
+    ii_4 = InvoiceItem.create!(invoice_id: invoice.id, item_id: item_4.id, quantity: 20, unit_price: 10, status: 1) # 200 = 475
+    discount_1 = merchant_1.bulk_discounts.create!(quantity: 10, percent: 25)
+    discount_2 = merchant_1.bulk_discounts.create!(quantity: 15, percent: 50)
+    
+    visit merchant_invoice_path(merchant_1, invoice)
+
+    within "#discounts" do
+      expect(page).to have_content("Total Revenue: $600.00")
+      expect(page).to have_content("Bulk Discount: $125.00")
+      expect(page).to have_content("Discounted Revenue: $475.00")
+    end
+  end
 end
